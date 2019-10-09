eccb6444cba6e8f461e3be813a20ccf6f9f4f61f
Dan094 <Dhanikamarasekera@gmail.com>
2019-10-09 10:24:18 -0700

    Added About us - Heidi and Nico

--- comps/aboutUs.js
+++ comps/aboutUs.js
@@ -4,16 +4,18 @@ import {View, Text, Image} from 'react-native';
 function AboutUs(){
     return(
         <View>
-            <Text>Hello, this is Heidi</Text>
+       
+            <Text>Hello,I am Heid</Text>
             <Image
             style={{width: 50, height: 50}}
           source={{uri:'https://siyayang0420.com/courton/wp-content/uploads/2019/09/Nicolas_Asis_Portrait.jpg'}}
             />
+            
             <Text>Hello, this is Nico</Text>
             <Image
             style={{width: 50, height: 50}}
           source={{uri:'https://siyayang0420.com/courton/wp-content/uploads/2019/09/Snapchat-26826338-e1569110534479.jpg'}}
-            />
+        />
         </View>
         
     )
